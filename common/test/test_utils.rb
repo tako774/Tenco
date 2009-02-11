f93c5e0a0fbcd60c229a161c7d5bc0c5a9eb9f35
nanashi <nanashi.hatch+redmine@gmail.com>
2009-02-11 16:45:16 +0900

    closes #10
    Utils.rb から iso8601_to_time を削除
    iso8601_to_time を使っていた部分を、Time.iso8601 に変更
    / nanashi

--- common/test/test_utils.rb
+++ common/test/test_utils.rb
@@ -32,12 +32,4 @@ class Test_Utils < Test::Unit::TestCase
 #{s "x' AND 1=(SELECT COUNT(*) FROM tabname); --"}
 		SQL
 	end
-	
-	def test_iso8601_to_time
-		time_str = "2008-12-06T21:11:19.234+09:00"
-		t1 = iso8601_to_time(time_str)
-		t2 = Time.parse(time_str)
-		assert_equal(t2, t1)
-	end
-	
 end
--- common/utils.rb
+++ common/utils.rb
@@ -13,39 +13,6 @@ module Utils
 	end
 	
 	# Time.parse が重過ぎるので代用のメソッド
-	# YYYY-MM-DDTHH:MM:SS[.fragments][[+-]MM:SS] を想定
-	def iso8601_to_time(iso8601)
-		if iso8601 =~ (/\A(.+?)([\+\-]\d{1,2}:\d{1,2})\z/) then
-			iso8601 = $1 + "TZ" + $2
-		end
-		
-		t, tz = iso8601.split(/TZ/)	
-		
-		t_shift = 0
-		if tz =~ /\A([\+\-])(\d{1,2}):(\d{1,2})\z/ then
-			t_shift = $2.to_i * 60 * 60 + $3.to_i * 60
-			if ($1 == "+") then
-				t_shift = -1 * t_shift
-			end
-		end
-		
-		t, frag = t.split(/[\.]/)
-		
-		# 秒以下があれば別途扱う
-		if frag then
-			frag = "0.#{frag}".to_f * 1000000
-		else
-			frag = 0
-		end
-		
-		ta = t.split(/[\/\- :T]/)
-		ta.push frag
-
-		return (Time.utc(*ta) + t_shift).getlocal
-	end
-	
-	
-	# Time.parse が重過ぎるので代用のメソッド
 	# YYYY-MM-DD HH:MM:SS[.fragments] を想定
 	def pgsql_timestamp_str_to_time(timestamp_str)
 		t, frag = timestamp_str.split(/[\.]/)
