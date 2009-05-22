e9e470a2fdde65b32af6e07acfcb6d286a8ce3f2
nanashi <nanashi.hatch+redmine@gmail.com>
2009-05-22 22:52:59 +0900

    対戦結果の検索をインクリメンタルでなく普通の検索に変えた
    マイページの対戦結果検索を、普通の検索に変更（重かったので）
    by nanashi

--- js/search.js
+++ js/search.js
@@ -1,17 +1,26 @@
-// インクリメンタルサーチ
-// 指定された各要素の配下のテキストを検索します 
-function simple_inc_search(target_element, display_style, str_element, delay) {
-	var target_element = target_element;
-	var display_style = display_style;
-	var str_element = str_element;
-	var delay = delay;
+// 検索
+// 指定された各要素の配下のテキストを検索します
+var lock_search_process = 0;
+var before_str = "";
 
-	var before_str = "";
+function search(target_element, display_style, str_element) {
+	if (lock_search_process == 0) {
+		lock_search_process = 1;
 	
-	setInterval(
-		function() {
-			var str = $(str_element).val();
-			if (str != before_str) {
+		var target_element = target_element;
+		var display_style = display_style;
+		var str_element = str_element;
+		
+		// 検索文字列取得
+		var str = $(str_element).val();
+		
+		if (str != before_str) {
+			var default_search_tool_msg = $("#search_tool_msg").text();
+			$("#search_tool_msg").text("検索中");
+			
+			before_str = str;
+			
+			if (str != "") {
 				// IE6 は show hide しか動かない 
 				if (jQuery.browser.msie) {
 					$(target_element).each(function() {
@@ -35,9 +44,23 @@ function simple_inc_search(target_element, display_style, str_element, delay) {
 					});
 				}
 			}
-			before_str = str;
-		},
-		delay
-	);
-	
+			else {
+				// IE6 は show hide しか動かない 
+				if (jQuery.browser.msie) {
+					$(target_element).show();
+				}
+				// IE 以外は css の display を操作するほうが体感高速 
+				else {
+					$(target_element).css("display", display_style);
+				}
+			}
+			
+			$("#search_tool_msg").text(default_search_tool_msg);		
+		}
+		
+		lock_search_process = 0;
+		
+		$(str_element).focus();
+		$(str_element).select();
+	}
 }	
