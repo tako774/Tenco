48c769b72854dc050d6d7a15fd1990056e63cb77
nanashi <nanashi.hatch@gmail.com>
2009-11-09 21:17:29 +0900

    403,404,503 ページ作成
    403.php : 404などからの罠リンク専用
    403.txt : 通常の403 アクセス制限時のページ兼用
    404.php : 通常の404 某所でみんなに書いてもらったレスがランダムで出るようにした
    503.html: 通常の503
    
    Signed-off-by: nanashi <nanashi.hatch@gmail.com>

---
+++ 403.php
@@ -0,0 +1,58 @@
+<?php
+	sleep(1);
+	header('HTTP/1.0 403 Forbidden', FALSE);
+	echo("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='ja' lang='ja'>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="content-style-type" content="text/css" />
+	<meta http-equiv="content-script-type" content="text/javascript" />
+	<link rel="stylesheet" type="text/css" href="/css/main.css" />	
+	<title>403 Forbidden - Tenco!</title>
+</head>
+<body>
+
+<div id="header">
+	<h1><a href="http://tenco.info/"><img alt="Tenco!" src='http://tenco.info/images/Tenco.gif' width='122' height='42' /></a></h1>
+	<p style="font-weight:bold">
+		403 Forbidden : このアドレスは見られません
+	</p>
+	<p id="meta_info">
+		λ＜このアドレスは見られません！
+	</p>
+</div>
+
+<hr />
+
+<div id="main">
+	<div class="section">
+		<pre class="AA">
+		
+　　　　　　　　　　 　　　　　　　　　　　 ,-､ ｎｎ
+　 　 　 　 　 r-、 _００　　　　 　 　 　 /::::'┴'ｒ'
+　 　 　 　 　 |::::'´::::r┘　　!「`L00、|.l└ｸ_;厂　 /
+　 　 　 　 　 |::::「|:::|　 　 l| |Lｎ:::r┘|.l _lﾆユ、 ./
+　 　 　 　 　 ￣└r''"´］_ l| | ｒﾞ=ﾞ┐ |└ァ::/　/　 /
+　　　　　　　､ヽ、 ,ゞ´_::::| l| |「二:::7 .|.l └′/　 / ／
+　　　 　 　 　 ＼＼｀´ |:::|. l| l　〈::/　 ､ !
+　　　　　　　　　　　　,.‐‐､　　　　　　 ,.-‐-､
+　　　　　　　　　　　く__,.ヘヽ.　　　　/　,ー､ 〉
+　　　　　　　　　　　　　＼ ', !-─‐-i　/　/´
+　　　　　　　　　　　 　 ／｀ｰ'　　　 L/／｀ヽ､
+　　　　　　　　　　 　 /　 ／,　 /|　 ,　 ,　　　 ' ,　
+　　　　　　　　　l⌒ヽl / /-‐/　ｉ　L_ ﾊ ヽ!　　 ヽ＿,-─-､
+　　　　　　　　　ヽ､　ﾚﾍ 7(ﾋ_]　　 ﾋ_ﾝ))ﾄ､!| l .|´::::::::|￣｀ヽ＼
+　　　　　　　　// 　＼!,/7oﾟ ,＿__,　 ﾟo iソ| | ||:::::::::::| ￣ヽ＼l
+バンバン　　　　　　　|.从　 ヽ _ﾝ　　 / |./ | ||:::::::::::| ＿　＼|＼＿__
+　　　　　☆　 | l l|,, ＿ﾚ'| i＞.､,,__ _,.イ /ノﾚﾚ:l:::::::::/ 　　ヽ､/＼＿__ ）_
+　　　　　　 ＼ゝ　　|::::::::::::::::::｀ヽ_（ ￣|:::::::::::::/￣ |￣￣ヽノヽ､ 　 　　 ）
+
+		</pre>
+	</div>
+</div>
+
+</body>
+</html>
+
