564adb81cf5f78c92e77c1e2a1d2ef783b3f74cc
nanashi <nanashi.hatch@gmail.com>
2013-05-12 11:42:04 +0900

    ゲームキャラDAO追加
    Signed-off-by: nanashi <nanashi.hatch@gmail.com>

---
+++ dao/GameType1Dao.rb
@@ -0,0 +1,33 @@
+require "#{File.expand_path(File.dirname(__FILE__))}/DaoBase"
+require "#{File.expand_path(File.dirname(__FILE__))}/../entity/GameType1"
+
+class GameType1Dao < DaoBase
+	@@version = 0.00
+	
+	# 指定されたゲームIDたちの情報をすべてかえす
+	def get_by_game_ids(game_ids)
+		game_type1s = {} # game_id => { type1_id => game_type1 }
+		
+		res = @db.exec(<<-"SQL")
+			SELECT
+        gt.*
+			FROM
+        game_type1s gt
+			WHERE
+			  gt.game_id IN (#{(game_ids.map { |i| "'#{i.to_i}'" } ).join(", ")})
+		SQL
+
+		res.each do |row|
+			gt = GameType1.new
+			res.num_fields.times do |i|
+				gt.instance_variable_set("@#{res.fields[i]}", row[i])
+			end
+			game_type1s[gt.game_id.to_i] ||= {}
+      game_type1s[gt.game_id.to_i][gt.type1_id.to_i] = gt
+		end
+		
+		res.clear
+			
+		return game_type1s
+	end
+end
