3b9c357b74b678ffd9e2c7083640261ddd42ef26
nanashi <nanashi.hatch@gmail.com>
2013-05-12 11:42:19 +0900

    リプレイDAO追加
    Signed-off-by: nanashi <nanashi.hatch@gmail.com>

---
+++ dao/ReplayDao.rb
@@ -0,0 +1,39 @@
+require "#{File.expand_path(File.dirname(__FILE__))}/DaoBase"
+require "#{File.expand_path(File.dirname(__FILE__))}/../entity/Replay"
+
+class ReplayDao < DaoBase
+  @@version = 0.00
+
+  # リプレイファイル情報を登録
+  def insert(replay)
+    inserted_replay = Replay.new
+    
+    res = @db.exec(<<-"SQL")
+      INSERT INTO
+        replays (
+          game_id
+        , track_record_id
+        , relative_file_path
+        , player1_account_id
+        , player1_type1_id
+        )
+      VALUES (
+        #{replay.game_id.to_i}
+      , #{replay.track_record_id.to_i}
+      , #{s replay.relative_file_path}
+      , #{replay.player1_account_id.to_i}
+      , #{replay.player1_type1_id.to_i}
+      )
+      RETURNING
+        *
+    SQL
+    
+    res.num_fields.times do |i|
+      inserted_replay.instance_variable_set("@#{res.fields[i]}", res[0][i])
+    end
+    
+    return inserted_replay
+  end
+  
+  
+end
